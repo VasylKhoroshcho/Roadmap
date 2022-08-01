3b8b5d479697b54bdc238f61793557a46ebe9030
VasylKhoroshcho <vasyl.khoroshcho@rolique.io>
2022-08-01 21:44:37 +0300

    Add tdz

---
+++ TDZ/index.js
@@ -0,0 +1,16 @@
+{
+ 	// This is the temporal dead zone for the age variable!
+	// This is the temporal dead zone for the age variable!
+	// This is the temporal dead zone for the age variable!
+ 	// This is the temporal dead zone for the age variable!
+	let available = true; // Whew, we got there! No more TDZ
+	console.log('available', available);
+}
+
+
+console.log(notAvailable)
+// This is the temporal dead zone for the age variable!
+// This is the temporal dead zone for the age variable!
+// This is the temporal dead zone for the age variable!
+// This is the temporal dead zone for the age variable!
+let notAvailable = false;
